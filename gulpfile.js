db93c3402bcb64e69ea0c81b5d3913a42e1251db
MIke Carter <mike@mcarter.me>
2015-03-27 13:59:08 +0000

    Live reloads working.

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,45 @@
 var gulp = require('gulp');
 var jade = require('gulp-jade');
 var watch = require('gulp-watch');
+var sass = require('gulp-sass');
+var connect = require('gulp-connect');
 
-var JADE_FILES = './code/*/*.jade';
+var JADE_FILES = './code/**/*.jade';
+var SASS_FILES = './code/**/*.scss';
+
+var BUILD_DEST = './dist/';
 
 gulp.task('templates', function() {
   gulp.src(JADE_FILES)
     .pipe(jade({
       pretty: true
     }))
-    .pipe(gulp.dest('./dist/'));
+    .pipe(gulp.dest(BUILD_DEST))
+    .pipe(connect.reload());
+});
+
+gulp.task('styles', function() {
+  gulp.src(SASS_FILES)
+    .pipe(sass())
+    .pipe(gulp.dest(BUILD_DEST))
+    .pipe(connect.reload());
 });
 
 gulp.task('watch', function () {
-    gulp.watch(JADE_FILES, function() {
-      gulp.run('templates');
-    });
+  gulp.watch(JADE_FILES, function() {
+    gulp.run('templates');
+  });
+
+  gulp.watch(SASS_FILES, function() {
+    gulp.run('styles');
+  });
 });
+
+gulp.task('connect', function() {
+  connect.server({
+    root: 'dist',
+    livereload: true
+  });
+});
+
+gulp.task('run', ['connect', 'watch']);
