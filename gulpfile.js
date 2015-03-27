02edbc4a9907108d09c4e19bfbbd2fff36d3727c
MIke Carter <mike@mcarter.me>
2015-03-27 15:23:43 +0000

    Got single page JS or pulling in of libs working.

--- gulpfile.js
+++ gulpfile.js
@@ -3,17 +3,32 @@ var jade = require('gulp-jade');
 var watch = require('gulp-watch');
 var sass = require('gulp-sass');
 var connect = require('gulp-connect');
+var browserify = require('gulp-browserify');
+var uglify = require('gulp-uglify');
+var open = require('gulp-open');
 
 var JADE_FILES = './code/**/*.jade';
 var SASS_FILES = './code/**/*.scss';
+var APP_JS_FILES = './code/scripts/app/**/*.js';
+var LIB_JS_FILES = './code/scripts/lib/**/*.js';
+var BROWSERIFY_ROOT = './code/scripts/app/main.js';
 
 var BUILD_DEST = './dist/';
 
+function logError (error) {
+  console.log(error.toString());
+  this.emit('end');
+}
+
+// ---------------------------------
+// --------- WATCH TASKS -----------
+// ---------------------------------
 gulp.task('templates', function() {
   gulp.src(JADE_FILES)
     .pipe(jade({
       pretty: true
     }))
+    .on('error', logError)
     .pipe(gulp.dest(BUILD_DEST))
     .pipe(connect.reload());
 });
@@ -21,8 +36,26 @@ gulp.task('templates', function() {
 gulp.task('styles', function() {
   gulp.src(SASS_FILES)
     .pipe(sass())
+    .on('error', logError)
     .pipe(gulp.dest(BUILD_DEST))
     .pipe(connect.reload());
+});
+
+gulp.task('app_scripts', function() {
+  gulp.src(BROWSERIFY_ROOT)
+    .pipe(browserify({
+      glboal: true,
+      debug : true
+    }))
+    .on('error', logError)
+    .pipe(gulp.dest(BUILD_DEST+'scripts/app/'));
+});
+
+gulp.task('lib_scripts', function() {
+  gulp.src(LIB_JS_FILES)
+    .pipe(uglify())
+    .on('error', logError)
+    .pipe(gulp.dest(BUILD_DEST+'scripts/lib/'));
 });
 
 gulp.task('watch', function () {
@@ -33,8 +66,19 @@ gulp.task('watch', function () {
   gulp.watch(SASS_FILES, function() {
     gulp.run('styles');
   });
+
+  gulp.watch(APP_JS_FILES, function() {
+    gulp.run('app_scripts');
+  });
+
+  gulp.watch(LIB_JS_FILES, function() {
+    gulp.run('lib_scripts');
+  });
 });
 
+// ----------------------------------
+// --------- SERVER TASKS -----------
+// ----------------------------------
 gulp.task('connect', function() {
   connect.server({
     root: 'dist',
@@ -42,4 +86,15 @@ gulp.task('connect', function() {
   });
 });
 
-gulp.task('run', ['connect', 'watch']);
+gulp.task('open', function(){
+  gulp.src('./dist/index.html')
+  .pipe(open('', {
+    url: 'http://localhost:8080',
+  }));
+});
+
+// ----------------------------------
+// --------- COMPOSITE TASKS -----------
+// ----------------------------------
+gulp.task('build', ['templates', 'styles', 'app_scripts', 'lib_scripts']);
+gulp.task('run', ['build', 'connect', 'watch', 'open']);
