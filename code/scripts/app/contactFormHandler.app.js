177d06f7c29ea51a72bd1ac0f550902e4e7a3b75
MIke Carter <mike@mcarter.me>
2015-04-16 22:33:35 +0100

    Form validation completed.

--- code/scripts/app/contactFormHandler.app.js
+++ code/scripts/app/contactFormHandler.app.js
@@ -2,55 +2,75 @@ var request = require('superagent');
 
 var CONTACT_FORM_ID = "contact-form";
 
-var attachHandler = function() {
+
+var utils = {
+  forEachChild: function(domElement, callback) {
+    var results = [];
+    for (var i=0; i < domElement.children.length; i++ ) {
+      results.push(callback(domElement.children[i]));
+    }
+    return results;
+  },
+  formElement: function() {
+    return document.getElementById(CONTACT_FORM_ID);
+  }
+}
+
+var attachHandler = function(options) {
+  var validations = options.validations || {};
+  var preSendHook = options.preSendHook || (function() {});
+
   var requestData = {
     email: null,
-    subject: "New contact!" ,
+    subject: null,
     body: null
   };
 
+  var otherData = {}
+
   var findInputForName = function(name) {
-    var formEl = document.getElementById(CONTACT_FORM_ID);
+    var result = null;
 
-    for (var i=0; i < formEl.children.length; i++ ) {
-      var formChildEl = formEl.children[i]
+    utils.forEachChild(utils.formElement(), function(childEl) {
+      if (childEl.getAttribute('name') == name) {
+        result = childEl;
+      }
+    });
 
-      if (formChildEl.getAttribute('name') == name) {
-        return formChildEl;
+    return result;
+  }
+
+  var clientValidate = function(domElement) {
+    if (Object.keys(validations).indexOf(domElement.name) !== -1) {
+      var message = validations[domElement.name](domElement);
+      if (message) {
+        addError(domElement, message);
+        return message;
       }
     }
-
     return null;
   }
 
-  var addToRequestData = function(domElement) {
+  var scrapeForData = function(domElement) {
     if (Object.keys(requestData).indexOf(domElement.name) !== -1) {
       requestData[domElement.name] = domElement.value;
+    } else {
+      otherData[domElement.name] = domElement.value;
     }
   };
 
   var clearFields = function() {
-    var formEl = document.getElementById(CONTACT_FORM_ID);
-
-    for (var i=0; i < formEl.children.length; i++ ) {
-      var formChildEl = formEl.children[i]
-
-      if (formChildEl.getAttribute('name')) {
-        formChildEl.value = '';
-      }
-    }
+    utils.forEachChild(utils.formElement(), function(childEl) {
+      if (childEl.getAttribute('name')) childEl.value = '';
+    });
   };
 
   var clearErrors = function() {
-    var formEl = document.getElementById(CONTACT_FORM_ID);
-
-    for (var i=0; i < formEl.children.length; i++ ) {
-      var formChildEl = formEl.children[i]
-
-      if (formChildEl.getAttribute('name') && /erroneous/.test(formChildEl.className)) {
-        clearError(formChildEl);
+    utils.forEachChild(utils.formElement(), function(childEl) {
+      if (childEl.getAttribute('name') && /erroneous/.test(childEl.className)) {
+        clearError(childEl);
       }
-    }
+    });
   };
 
   var clearError = function(focusEvent) {
@@ -59,19 +79,22 @@ var attachHandler = function() {
     domEl.parentNode.removeChild(domEl.nextSibling);
   }
 
+  var addError = function(domEl, message) {
+    if (/erroneous/.test(domEl.className)) return;
+
+    var errorEl = document.createElement("SPAN");
+    errorEl.className = "error";
+    domEl.className += " erroneous";
+    errorEl.appendChild(document.createTextNode(message));
+    domEl.parentNode.insertBefore(errorEl, domEl.nextSibling);
+
+    domEl.addEventListener('focus', clearError);
+  };
+
   var showErrors = function(errors) {
     for (var key in errors) {
       var domEl = findInputForName(key);
-
-      if (!domEl || /erroneous/.test(domEl.className)) continue;
-
-      var errorEl = document.createElement("SPAN");
-      errorEl.className = "error";
-      domEl.className += " erroneous";
-      errorEl.appendChild(document.createTextNode(errors[key]));
-      domEl.parentNode.insertBefore(errorEl, domEl.nextSibling);
-
-      domEl.addEventListener('focus', clearError);
+      if (domEl) addError(domEl, errors[key]);
     }
   }
 
@@ -84,21 +107,18 @@ var attachHandler = function() {
     alert("Success!");
   };
 
-  var attemptSendEmail = function() {
+  var onFormSubmit = function(submitEvent) {
+    submitEvent.preventDefault();
+
+    var errorStrings = utils.forEachChild(utils.formElement(), clientValidate);
+    if (errorStrings.join('').length) return;
+    
+    utils.forEachChild(utils.formElement(), scrapeForData);
+    preSendHook(requestData, otherData);
+    
     request.post('http://leaf-api.herokuapp.com/v1/contact')
       .send({message:requestData})
       .end(handleSendEmailResponse);
-  };
-
-  var onFormSubmit = function(submitEvent) {
-    submitEvent.preventDefault();
-    var formChildren = submitEvent.target.children;
-
-    for (var i=0; i < formChildren.length; i++ ) {
-      addToRequestData(formChildren[i]);
-    }
-
-    attemptSendEmail();
   };
 
   window.onload = function() {
@@ -106,4 +126,23 @@ var attachHandler = function() {
   };
 };
 
-attachHandler();
+
+attachHandler({
+  validations: {
+    name: function(input) {
+      if (!input.value) return "Must be provided."
+    },
+    email: function(input) {
+      if (!input.value) return "Must be a valid email.";
+    },
+    body: function(input) {
+      if (!input.value) return "Must be provided.";
+    }
+  },
+  preSendHook: function(requestData, otherData) {
+    requestData.subject = "New contact from " + otherData.name;
+    if (requestData.body) {
+      requestData.body += ("\n\n Budget: " + otherData.budget);
+    }
+  }
+});
