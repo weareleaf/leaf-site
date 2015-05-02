faab2715f8f5f36977f73243e862c40e14e1765b
MIke Carter <mike@mcarter.me>
2015-05-02 13:49:33 +0100

    Switch PNGs for SVG

--- code/scripts/app/logoAnimator.app.js
+++ code/scripts/app/logoAnimator.app.js
@@ -1,5 +1,3 @@
 var Vivus = require('vivus');
 
-new Vivus('animated-logo', {type: 'delayed', duration: 50, file: 'images/logo.svg'}, function() {
-  console.log(arguments);
-});
+new Vivus('animated-logo', {type: 'delayed', duration: 50, file: 'images/leaf.svg'});
