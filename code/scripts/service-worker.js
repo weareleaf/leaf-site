843a864e454aec6656b1761a05502c1fabc4cd45
Mike Carter <mike@weareleaf.com>
2017-05-19 15:18:57 +0100

    Fix service worker issues

--- code/scripts/service-worker.js
+++ code/scripts/service-worker.js
@@ -5,34 +5,38 @@ var urlsToCache = [
 ]
 
 self.addEventListener('install', function(event) {
-  const cacheOpened = caches.open(CACHE_NAME)
-    .then(function(cache) {
-      return cache.addAll(urlsToCache)
+  console.log('The service worker is being installed.');
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(urlsToCache);
     })
-  event.waitUntil(cacheOpened)
-})
+  )
+});
 
 self.addEventListener('fetch', function(event) {
-  var matchedCaches = caches.match(event.request).then(function(resp) {
-      return resp || fetch(event.request).then(function(response) {
-        return caches.open(CACHE_NAME).then(function(cache) {
-          cache.put(event.request, response.clone())
-          return response
-        })
-      })
+  console.log('The service worker is serving the asset.');
+  event.respondWith(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.match(event.request).then(function (response) {
+        return response || fetch(event.request).then(function(response) {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      });
     })
-  event.respondWith(matchedCaches)
-})
+  );
+});
 
 self.addEventListener('activate', function(event) {
-  const otherCachesDeleted = caches.keys()
-    .then(function(cacheKeys) {
-      const promisedCacheDeletions = cacheKeys.map(function(cacheKey) {
-        if (CACHE_NAME !== cacheKey) {
-          return caches.delete(cacheKey)
-        }
-      })
-      return Promise.all(promisedCacheDeletions)
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName !== CACHE_NAME;
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
     })
-  event.waitUntil(otherCachesDeleted)
-})
+  );
+});
