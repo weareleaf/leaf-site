12c46480703f72ca3931c2e404d562b337d2e0f9
Mike Carter <mike@weareleaf.com>
2018-12-21 16:37:58 +0000

    Further optimisations including image preloading

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -20,3 +20,39 @@ if(window.navigator && navigator.serviceWorker) {
       console.log("All service workers unregistered.")
     })
 }
+
+function isMobileDevice() {
+  var agent = navigator.userAgent
+  return agent.match(/Android/i) || agent.match(/webOS/i) || agent.match(/iPhone/i) || agent.match(/iPad/i) || agent.match(/iPod/i) || agent.match(/BlackBerry/i) || agent.match(/Windows Phone/i)
+}
+
+
+function canUseWebP() {
+  var elem = document.createElement('canvas');
+  if (!!(elem.getContext && elem.getContext('2d'))) {
+    return elem.toDataURL('image/webp').indexOf('data:image/webp') == 0
+  }
+  return false
+}
+
+function preload(src) {
+  console.log('Preloading', src)
+  var i = new Image()
+  i.src = src
+}
+
+if (!isMobileDevice()) {
+  window.addEventListener('load', function() {
+    var ext = canUseWebP() ? 'webp' : 'png'
+
+    preload('/assets/images/what-we-do.' + ext)
+    preload('/assets/images/company.' + ext)
+    preload('/assets/images/home.' + ext)
+    preload('/assets/images/ebooks.' + ext)
+    preload('/assets/images/jobs.' + ext)
+    preload('/assets/images/our-work.' + ext)
+    preload('/assets/images/project-planner.' + ext)
+    preload('/assets/images/resources.' + ext)
+    preload('/assets/images/terms.' + ext)
+  })
+}
