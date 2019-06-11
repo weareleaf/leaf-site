3ed15d7f8e62238af85d86dd6f210018c4c4c1fa
Mike Carter <mike@weareleaf.com>
2019-06-11 12:05:23 +0100

    Fix blog authors

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
 const glob = require('glob')
-
 const pages = glob.sync('**/*.pug', {
   cwd: path.resolve(__dirname, 'src/pages')
 })
