cbfccc04e66e6d6568e3a580b5febfba752162d7
Mike Carter <mike@weareleaf.com>
2019-06-18 12:04:10 +0100

    Add leaf logo to site

--- src/assets/scripts/banner.js
+++ src/assets/scripts/banner.js
@@ -9,7 +9,9 @@ function onClick() {
 }
 
 export default function () {
-  const bannerButton = document.querySelector(".banner__button");
+  const bannerButton = document.querySelector('.banner__button')
+  const bannerLogo = document.querySelector('.banner__logo')
+  const bannerImage = document.querySelector('.banner__image')
 
   if (bannerButton) {
     const { pathname, origin } = window.location
@@ -19,4 +21,22 @@ export default function () {
       bannerButton.href = '/our-work'
     }
   }
+
+  if (bannerLogo) {
+    const fadeInOffset = bannerImage.offsetHeight
+    let visible = false
+
+    window.addEventListener('scroll', (e) => {
+      const offset = document.body.scrollTop
+
+      if (offset >= fadeInOffset && !visible) {
+        bannerLogo.classList.add('banner__logo--visible')
+        visible = true
+      } else if (offset < fadeInOffset && visible) {
+        bannerLogo.classList.remove('banner__logo--visible')
+        visible = false
+      }
+    })
+  }
+
 }
