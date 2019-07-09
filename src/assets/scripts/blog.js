d65b7097c24d1a45b6ea3c70554818f3a7f4e7a6
Mike Carter <mike@weareleaf.com>
2019-07-09 17:14:30 +0100

    Add process map blog post

--- src/assets/scripts/blog.js
+++ src/assets/scripts/blog.js
@@ -8,6 +8,26 @@ import BlogPostGrid from './components/blog/BlogPostGrid'
 const gridItems = [
   {
     author: {
+      authorName: 'Mike Carter',
+      authorDetails: '10 July 2019 · 6 min read',
+      authorThumbnail: {
+        src: '/assets/images/mike_blog.jpg',
+        fallbackSrc: '/assets/images/mike_blog.jpg',
+        alt: 'Mike'
+      }
+    },
+    postUrl: '/blog/automate-where-it-matters-with-process-maps',
+    postImage: {
+      src: '/assets/images/blog/thumbnails/walkers-hill.jpg',
+      fallbackSrc: '/assets/images/blog/thumbnails/walkers-hill.jpg',
+      alt: 'Automate where it matters with process maps'
+    },
+    postHeading: 'Automate where it matters with process maps',
+    postText:
+      'How process maps can be used to get concrete shared understanding of a business for smart decision making around automation.'
+  },
+  {
+    author: {
       authorName: 'Curtis Lee',
       authorDetails: '19 June 2019 · 4 min read',
       authorThumbnail: {
