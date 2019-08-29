2246a29c9d52c20f4642a2d33a64f03c444e8c7e
Curtis A Lee <leecurtis353@yahoo.co.uk>
2019-08-29 13:36:45 +0100

    npm issue - also going to attempt the logo fade in.

---
+++ src/assets/scripts/emailButton.js
@@ -0,0 +1,42 @@
+function onClick() {
+  const { pathname, origin } = window.location
+
+  if (pathname.match(/blog/)) {
+    window.location = `${origin}/blog`
+  } else {
+    window.location = `${origin}/our-work`
+  }
+}
+
+export default function () {
+  const bannerButton = document.querySelector('.banner__button')
+  const bannerLogo = document.querySelector('.banner__logo')
+  const bannerImage = document.querySelector('.banner__image')
+
+  if (bannerButton) {
+    const { pathname, origin } = window.location
+    if (pathname.match(/blog/)) {
+      bannerButton.href = '/blog'
+    } else {
+      bannerButton.href = '/our-work'
+    }
+  }
+
+  if (bannerLogo) {
+    const fadeInOffset = bannerImage.offsetHeight
+    let visible = false
+
+    window.addEventListener('scroll', (e) => {
+      const offset = window.scrollY
+
+      if (offset >= fadeInOffset && !visible) {
+        bannerLogo.classList.add('banner__logo--visible')
+        visible = true
+      } else if (offset < fadeInOffset && visible) {
+        bannerLogo.classList.remove('banner__logo--visible')
+        visible = false
+      }
+    })
+  }
+
+}
