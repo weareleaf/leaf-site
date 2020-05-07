f1b4806175ffaefe18cdb3078395c8d597c4a8a3
Mike Carter <mike@weareleaf.com>
2020-05-07 14:15:35 +0100

    Update from SEO feedback

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,10 @@
+const { generateSitemapConfig } = require('./scripts/sitemap')
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
+const SitemapPlugin = require('sitemap-webpack-plugin').default
 const glob = require('glob')
+
 const pages = glob.sync('**/*.pug', {
   cwd: path.resolve(__dirname, 'src/pages')
 })
@@ -12,6 +15,7 @@ const pagePlugins = pages.map(page => {
     filename: page.replace('.pug', '.html')
   })
 })
+
 const filePlugins = new CopyPlugin([
   { from: './src/assets/fonts/', to: './assets/fonts' },
   { from: './src/assets/images/', to: './assets/images/' },
@@ -19,13 +23,35 @@ const filePlugins = new CopyPlugin([
   { from: './src/robots.txt', to: './robots.txt' }
 ])
 
+const sitemapPlugin = new SitemapPlugin(
+  'https://weareleaf.com',
+  generateSitemapConfig(), {
+    filename: 'sitemap.xml'
+  }
+)
+
+const statsConfig = {
+  children: false,
+  chunks: false,
+  assets: false,
+  builtAt: false,
+  hash: false,
+  // timings: false,
+  entrypoints: false,
+  modules: false,
+  version: false,
+  warnings: false,
+}
+
 const config = {
+  stats: statsConfig,
   entry: {
-    app: './src/assets/scripts/main.js'
+    app: './src/assets/scripts/main.js',
+    css: './src/assets/styles/main.scss'
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js',
+    filename: '[name].js',
     publicPath: '/'
   },
   devServer: {
@@ -45,19 +71,9 @@ const config = {
         }
       ]
     },
-    stats: {
-      children: false,
-      chunks: false,
-      assets: false,
-      builtAt: false,
-      hash: false,
-      // timings: false,
-      entrypoints: false,
-      modules: false,
-      version: false,
-    },
+    stats: statsConfig,
   },
-  plugins: [].concat(pagePlugins, filePlugins),
+  plugins: [].concat(pagePlugins, filePlugins, sitemapPlugin),
   module: {
     rules: [
       {
@@ -74,6 +90,21 @@ const config = {
             plugins: ['@babel/plugin-proposal-object-rest-spread']
           }
         }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'assets/styles/[name].css',
+            }
+          },
+          { loader: 'extract-loader' },
+          { loader: 'css-loader?-url' },
+          { loader: 'postcss-loader' },
+          { loader: 'sass-loader' }
+        ]
       }
     ]
   }
