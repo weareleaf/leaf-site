978ec38d3cf4995c469c04f7b5a84057ae3f12ff
Oliver Standeven <ojsdude@users.noreply.github.com>
2021-01-13 17:19:22 +0000

    Review changes

--- src/assets/scripts/stripe.js
+++ src/assets/scripts/stripe.js
@@ -5,26 +5,31 @@ const addPaymentButtonListeners = function() {
   const productPrices = {
     'analysis': liveSite ? 'price_1I8p39C2yPQt7QIdJCmWYM6j' : 'price_1I8pChC2yPQt7QIdSvx88QFs'
   }
-  const paymentButtons = document.querySelectorAll('button[product][success][cancel]')
+  const paymentButtons = document.querySelectorAll('button[purchase-product]')
   for (let i = 0; i < paymentButtons.length; i++) {
     let button = paymentButtons[i]
     button.addEventListener('click', function () {
-      const product = this.getAttribute("product")
-      const success = this.getAttribute("success")
-      const cancel = this.getAttribute("cancel")
+      this.classList.add("loading")
+      const product = this.getAttribute("purchase-product")
+      const productUrl = window.location.origin + "/" + product
+      const successUrl = productUrl + "/purchase-complete"
+      const errorUrl = productUrl + "/purchase-failed"
       stripe.redirectToCheckout({
         lineItems: [{
           price: productPrices[product],
           quantity: 1
         }],
         mode: 'payment',
-        successUrl: window.location.origin + success,
-        cancelUrl: window.location.origin + cancel,
+        successUrl: successUrl,
+        cancelUrl: productUrl,
       })
       .then(function (result) {
         if (result.error) {
-          console.error(result.error.message)
+          window.location = errorUrl
         }
+      })
+      .catch(function () {
+        window.location = errorUrl
       })
     })
   }
